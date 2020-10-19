a0be1dd5650cc316d1e775f4a62d8106cbc68caf
qitas <qitas@qitas.cn>
2020-10-19 19:30:27 +0800

    init

--- src/XPT2046.cpp
+++ src/XPT2046.cpp
@@ -1,219 +1,193 @@
-/* Touchscreen library for XPT2046 Touch Controller Chip
- * Copyright (c) 2015, Paul Stoffregen, paul@pjrc.com
+/*
+ * Copyright (c) 2015-2016  Spiros Papadimitriou
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This file is part of github.com/spapadim/XPT2046 and is released
+ * under the MIT License: https://opensource.org/licenses/MIT
  *
- * The above copyright notice, development funding notice, and this permission
- * notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This software is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.
  */
+
+#include <Arduino.h>
+#include <SPI.h>
 
 #include "XPT2046.h"
 
-#define Z_THRESHOLD     400
-#define Z_THRESHOLD_INT	75
-#define MSEC_THRESHOLD  3
-#define SPI_SETTING     SPISettings(2000000, MSBFIRST, SPI_MODE0)
 
-static XPT2046_Touchscreen 	*isrPinptr;
-void isrPin(void);
-
-bool XPT2046_Touchscreen::begin(SPIClass &wspi)
-{
-	_pspi = &wspi;
-	_pspi->begin();
-	pinMode(csPin, OUTPUT);
-	digitalWrite(csPin, HIGH);
-	if (255 != tirqPin) {
-		pinMode( tirqPin, INPUT );
-		attachInterrupt(digitalPinToInterrupt(tirqPin), isrPin, FALLING);
-		isrPinptr = this;
-	}
-	return true;
+inline static void swap(uint16_t &a, uint16_t &b) {
+  uint16_t tmp = a;
+  a = b;
+  b = tmp;
 }
 
-#if defined(_FLEXIO_SPI_H_)
-#define FLEXSPI_SETTING     FlexIOSPISettings(2000000, MSBFIRST, SPI_MODE0)
-bool XPT2046_Touchscreen::begin(FlexIOSPI &wflexspi)
-{
-	_pspi = nullptr; // make sure we dont use this one...
-	_pflexspi = &wflexspi;
-	_pflexspi->begin();
-	pinMode(csPin, OUTPUT);
-	digitalWrite(csPin, HIGH);
-	if (255 != tirqPin) {
-		pinMode( tirqPin, INPUT );
-		attachInterrupt(digitalPinToInterrupt(tirqPin), isrPin, FALLING);
-		isrPinptr = this;
-	}
-	return true;
+/**********************************************************/
+#if 0
+
+// Bisection-based median; will modify vals array
+static uint16_t fastMedian (uint16_t *vals, uint8_t n) {
+  uint8_t l = 0, r = n;
+
+  while (l < r) {
+    uint16_t pivot = vals[l];  // TODO use middle elt?
+    uint8_t i = l+1, j = r-1;
+    while (i <= j) {
+      while ((i < r) && (vals[i] <= pivot)) {
+        ++i;
+      }
+      while ((j > l) && (vals[j] > pivot)) {
+        --j;
+      }
+      if (i < j) {
+        swap(vals[i], vals[j]);
+      }
+    }
+    swap(vals[l], vals[j]);
+
+    // j is final pivot position
+    if (j == n/2) {
+      return vals[j];
+    } else if (n/2 < j) {
+      r = i;
+    } else { // n/2 > j
+      l = j+1;
+    }
+  }
 }
+
+static uint16_t mean (const uint16_t *vals, uint8_t n) {
+  uint32_t sum = 0;
+  for (uint8_t i = 0;  i < n;  i++) {
+    sum += vals[i];
+  }
+  return (uint16_t)(sum/n);
+}
+
+#endif // 0
+/**********************************************************/
+
+XPT2046::XPT2046 (uint8_t cs_pin, uint8_t irq_pin) 
+: _cs_pin(cs_pin), _irq_pin(irq_pin) {
+}
+
+void XPT2046::begin(uint16_t width, uint16_t height) {
+  pinMode(_cs_pin, OUTPUT);
+  pinMode(_irq_pin, INPUT_PULLUP);
+
+  _width = width;
+  _height = height;
+
+  // Default calibration (map 0..ADC_MAX -> 0..width|height)
+  setCalibration(
+    /*vi1=*/((int32_t)CAL_MARGIN) * ADC_MAX / width,
+    /*vj1=*/((int32_t)CAL_MARGIN) * ADC_MAX / height,
+    /*vi2=*/((int32_t)width - CAL_MARGIN) * ADC_MAX / width,
+    /*vj2=*/((int32_t)height - CAL_MARGIN) * ADC_MAX / height
+  );
+  // TODO(?) Use the following empirical calibration instead? -- Does it depend on VCC??
+  // touch.setCalibration(209, 1759, 1775, 273);
+
+  SPI.begin();
+
+  powerDown();  // Make sure PENIRQ is enabled
+}
+
+void XPT2046::getCalibrationPoints(uint16_t &x1, uint16_t &y1, uint16_t &x2, uint16_t &y2) {
+  x1 = y1 = CAL_MARGIN;
+  x2 = _width - CAL_MARGIN;
+  y2 = _height - CAL_MARGIN;
+}
+
+void XPT2046::setCalibration (uint16_t vi1, uint16_t vj1, uint16_t vi2, uint16_t vj2) {
+  _cal_dx = _width - 2*CAL_MARGIN;
+  _cal_dy = _height - 2*CAL_MARGIN;
+
+  _cal_vi1 = (int32_t)vi1;
+  _cal_vj1 = (int32_t)vj1;
+  _cal_dvi = (int32_t)vi2 - vi1;
+  _cal_dvj = (int32_t)vj2 - vj1;
+}
+
+uint16_t XPT2046::_readLoop(uint8_t ctrl, uint8_t max_samples) const {
+  uint16_t prev = 0xffff, cur = 0xffff;
+  uint8_t i = 0;
+  do {
+    prev = cur;
+    cur = SPI.transfer(0);
+    cur = (cur << 4) | (SPI.transfer(ctrl) >> 4);  // 16 clocks -> 12-bits (zero-padded at end)
+  } while ((prev != cur) && (++i < max_samples));
+//Serial.print("RL i: "); Serial.println(i); Serial.flush();  // DEBUG
+  return cur;
+}
+
+// TODO: Caveat - MODE_SER is completely untested!!
+//   Need to measure current draw and see if it even makes sense to keep it as an option
+void XPT2046::getRaw (uint16_t &vi, uint16_t &vj, adc_ref_t mode, uint8_t max_samples) const {
+  // Implementation based on TI Technical Note http://www.ti.com/lit/an/sbaa036/sbaa036.pdf
+
+  uint8_t ctrl_lo = ((mode == MODE_DFR) ? CTRL_LO_DFR : CTRL_LO_SER);
+  
+  digitalWrite(_cs_pin, LOW);
+  SPI.transfer(CTRL_HI_X | ctrl_lo);  // Send first control byte
+  vi = _readLoop(CTRL_HI_X | ctrl_lo, max_samples);
+  vj = _readLoop(CTRL_HI_Y | ctrl_lo, max_samples);
+
+  if (mode == MODE_DFR) {
+    // Turn off ADC by issuing one more read (throwaway)
+    // This needs to be done, because PD=0b11 (needed for MODE_DFR) will disable PENIRQ
+    SPI.transfer(0);  // Maintain 16-clocks/conversion; _readLoop always ends after issuing a control byte
+    SPI.transfer(CTRL_HI_Y | CTRL_LO_SER);
+  }
+  SPI.transfer16(0);  // Flush last read, just to be sure
+  
+  digitalWrite(_cs_pin, HIGH);
+}
+
+void XPT2046::getPosition (uint16_t &x, uint16_t &y, adc_ref_t mode, uint8_t max_samples) const {
+  if (!isTouching()) {
+    x = y = 0xffff;
+    return;
+  }
+
+  uint16_t vi, vj;
+  getRaw(vi, vj, mode, max_samples);
+
+  // Map to (un-rotated) display coordinates
+#if defined(SWAP_AXES) && SWAP_AXES
+  x = (uint16_t)(_cal_dx * (vj - _cal_vj1) / _cal_dvj + CAL_MARGIN);
+  y = (uint16_t)(_cal_dy * (vi - _cal_vi1) / _cal_dvi + CAL_MARGIN);
+#else
+  x = (uint16_t)(_cal_dx * (vi - _cal_vi1) / _cal_dvi + CAL_MARGIN);
+  y = (uint16_t)(_cal_dy * (vj - _cal_vj1) / _cal_dvj + CAL_MARGIN);
 #endif
 
-
-ISR_PREFIX
-void isrPin( void )
-{
-	XPT2046_Touchscreen *o = isrPinptr;
-	o->isrWake = true;
+  // Transform based on current rotation setting
+  // TODO: Is it worth to do this by tweaking _cal_* values instead?
+  switch (_rot) {  // TODO double-check
+  case ROT90:
+    x = _width - x;
+    swap(x, y);
+    break;
+  case ROT180:
+    x = _width - x;
+    y = _height - y; 
+    break;
+  case ROT270:
+    y = _height - y; 
+    swap(x, y);
+    break;
+  case ROT0:
+  default:
+    // Do nothing
+    break;
+  }
 }
 
-TS_Point XPT2046_Touchscreen::getPoint()
-{
-	update();
-	return TS_Point(xraw, yraw, zraw);
+void XPT2046::powerDown() const {
+  digitalWrite(_cs_pin, LOW);
+  // Issue a throw-away read, with power-down enabled (PD{1,0} == 0b00)
+  // Otherwise, ADC is disabled
+  SPI.transfer(CTRL_HI_Y | CTRL_LO_SER);
+  SPI.transfer16(0);  // Flush, just to be sure
+  digitalWrite(_cs_pin, HIGH);
 }
-
-bool XPT2046_Touchscreen::tirqTouched()
-{
-	return (isrWake);
-}
-
-bool XPT2046_Touchscreen::touched()
-{
-	update();
-	return (zraw >= Z_THRESHOLD);
-}
-
-void XPT2046_Touchscreen::readData(uint16_t *x, uint16_t *y, uint8_t *z)
-{
-	update();
-	*x = xraw;
-	*y = yraw;
-	*z = zraw;
-}
-
-bool XPT2046_Touchscreen::bufferEmpty()
-{
-	return ((millis() - msraw) < MSEC_THRESHOLD);
-}
-
-static int16_t besttwoavg( int16_t x , int16_t y , int16_t z ) {
-  int16_t da, db, dc;
-  int16_t reta = 0;
-  if ( x > y ) da = x - y; else da = y - x;
-  if ( x > z ) db = x - z; else db = z - x;
-  if ( z > y ) dc = z - y; else dc = y - z;
-
-  if ( da <= db && da <= dc ) reta = (x + y) >> 1;
-  else if ( db <= da && db <= dc ) reta = (x + z) >> 1;
-  else reta = (y + z) >> 1;   //    else if ( dc <= da && dc <= db ) reta = (x + y) >> 1;
-
-  return (reta);
-}
-
-// TODO: perhaps a future version should offer an option for more oversampling,
-//       with the RANSAC algorithm https://en.wikipedia.org/wiki/RANSAC
-
-void XPT2046_Touchscreen::update()
-{
-	int16_t data[6];
-	int z;
-	if (!isrWake) return;
-	uint32_t now = millis();
-	if (now - msraw < MSEC_THRESHOLD) return;
-	if (_pspi) {
-		_pspi->beginTransaction(SPI_SETTING);
-		digitalWrite(csPin, LOW);
-		_pspi->transfer(0xB1 /* Z1 */);
-		int16_t z1 = _pspi->transfer16(0xC1 /* Z2 */) >> 3;
-		z = z1 + 4095;
-		int16_t z2 = _pspi->transfer16(0x91 /* X */) >> 3;
-		z -= z2;
-		if (z >= Z_THRESHOLD) {
-			_pspi->transfer16(0x91 /* X */);  // dummy X measure, 1st is always noisy
-			data[0] = _pspi->transfer16(0xD1 /* Y */) >> 3;
-			data[1] = _pspi->transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
-			data[2] = _pspi->transfer16(0xD1 /* Y */) >> 3;
-			data[3] = _pspi->transfer16(0x91 /* X */) >> 3;
-		}
-		else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
-		data[4] = _pspi->transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
-		data[5] = _pspi->transfer16(0) >> 3;
-		digitalWrite(csPin, HIGH);
-		_pspi->endTransaction();
-	}
-#if defined(_FLEXIO_SPI_H_)
-	else if (_pflexspi) {
-		_pflexspi->beginTransaction(FLEXSPI_SETTING);
-		digitalWrite(csPin, LOW);
-		_pflexspi->transfer(0xB1 /* Z1 */);
-		int16_t z1 = _pflexspi->transfer16(0xC1 /* Z2 */) >> 3;
-		z = z1 + 4095;
-		int16_t z2 = _pflexspi->transfer16(0x91 /* X */) >> 3;
-		z -= z2;
-		if (z >= Z_THRESHOLD) {
-			_pflexspi->transfer16(0x91 /* X */);  // dummy X measure, 1st is always noisy
-			data[0] = _pflexspi->transfer16(0xD1 /* Y */) >> 3;
-			data[1] = _pflexspi->transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
-			data[2] = _pflexspi->transfer16(0xD1 /* Y */) >> 3;
-			data[3] = _pflexspi->transfer16(0x91 /* X */) >> 3;
-		}
-		else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
-		data[4] = _pflexspi->transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
-		data[5] = _pflexspi->transfer16(0) >> 3;
-		digitalWrite(csPin, HIGH);
-		_pflexspi->endTransaction();
-
-	}
-#endif
-	// If we do not have either _pspi or _pflexspi than bail.
-	else return;
-
-	//Serial.printf("z=%d  ::  z1=%d,  z2=%d  ", z, z1, z2);
-	if (z < 0) z = 0;
-	if (z < Z_THRESHOLD) { //	if ( !touched ) {
-		// Serial.println();
-		zraw = 0;
-		if (z < Z_THRESHOLD_INT) { //	if ( !touched ) {
-			if (255 != tirqPin) isrWake = false;
-		}
-		return;
-	}
-	zraw = z;
-
-	// Average pair with least distance between each measured x then y
-	//Serial.printf("    z1=%d,z2=%d  ", z1, z2);
-	//Serial.printf("p=%d,  %d,%d  %d,%d  %d,%d", zraw,
-		//data[0], data[1], data[2], data[3], data[4], data[5]);
-	int16_t x = besttwoavg( data[0], data[2], data[4] );
-	int16_t y = besttwoavg( data[1], data[3], data[5] );
-
-	//Serial.printf("    %d,%d", x, y);
-	//Serial.println();
-	if (z >= Z_THRESHOLD) {
-		msraw = now;	// good read completed, set wait
-		switch (rotation) {
-		  case 0:
-			xraw = 4095 - y;
-			yraw = x;
-			break;
-		  case 1:
-			xraw = x;
-			yraw = y;
-			break;
-		  case 2:
-			xraw = y;
-			yraw = 4095 - x;
-			break;
-		  default: // 3
-			xraw = 4095 - x;
-			yraw = 4095 - y;
-		}
-	}
-}
-
-
 
--- src/XPT2046.h
+++ src/XPT2046.h
@@ -1,89 +1,61 @@
-/* Touchscreen library for XPT2046 Touch Controller Chip
- * Copyright (c) 2015, Paul Stoffregen, paul@pjrc.com
+/*
+ * Copyright (c) 2015-2016  Spiros Papadimitriou
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This file is part of github.com/spapadim/XPT2046 and is released
+ * under the MIT License: https://opensource.org/licenses/MIT
  *
- * The above copyright notice, development funding notice, and this permission
- * notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * This software is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied.
  */
 
 #ifndef _XPT2046_h_
 #define _XPT2046_h_
 
-#include "Arduino.h"
-#include <SPI.h>
-
-#if defined(__IMXRT1062__)
-#if __has_include(<FlexIOSPI.h>)
-	#include <FlexIOSPI.h>
-#endif
+// On my display driver ICs i,j mapped to (width-y),x
+//  Flipping can be handled by order of calibration points, but not swapping
+#if !defined(SWAP_AXES)
+#  define SWAP_AXES 1
 #endif
 
-#if ARDUINO < 10600
-#error "Arduino 1.6.0 or later (SPI library) is required"
-#endif
-
-class TS_Point {
+class XPT2046 {
 public:
-	TS_Point(void) : x(0), y(0), z(0) {}
-	TS_Point(int16_t x, int16_t y, int16_t z) : x(x), y(y), z(z) {}
-	bool operator==(TS_Point p) { return ((p.x == x) && (p.y == y) && (p.z == z)); }
-	bool operator!=(TS_Point p) { return ((p.x != x) || (p.y != y) || (p.z != z)); }
-	int16_t x, y, z;
-};
+  static const uint16_t CAL_MARGIN = 20;
 
-class XPT2046_Touchscreen {
-public:
-	constexpr XPT2046_Touchscreen(uint8_t cspin, uint8_t tirq=255)
-		: csPin(cspin), tirqPin(tirq) { }
-	bool begin(SPIClass &wspi = SPI);
-#if defined(_FLEXIO_SPI_H_)
-	bool begin(FlexIOSPI &wflexspi);
-#endif
+  enum rotation_t : uint8_t { ROT0, ROT90, ROT180, ROT270 };
+  enum adc_ref_t : uint8_t { MODE_SER, MODE_DFR };
 
-	TS_Point getPoint();
-	bool tirqTouched();
-	bool touched();
-	void readData(uint16_t *x, uint16_t *y, uint8_t *z);
-	bool bufferEmpty();
-	uint8_t bufferSize() { return 1; }
-	void setRotation(uint8_t n) { rotation = n % 4; }
-// protected:
-	volatile bool isrWake=true;
+  XPT2046 (uint8_t cs_pin, uint8_t irq_pin);
+
+  void begin(uint16_t width, uint16_t height);  // width and height with no rotation!
+  void setRotation(rotation_t rot) { _rot = rot; }
+
+  // Calibration needs to be done with no rotation, on both display and touch drivers
+  void getCalibrationPoints(uint16_t &x1, uint16_t &y1, uint16_t &x2, uint16_t &y2);
+  void setCalibration (uint16_t vi1, uint16_t vj1, uint16_t vi2, uint16_t vj2);
+
+  bool isTouching() const { return (digitalRead(_irq_pin) == LOW); }
+
+  void getRaw(uint16_t &vi, uint16_t &vj, adc_ref_t mode = MODE_DFR, uint8_t max_samples = 0xff) const;
+  void getPosition(uint16_t &x, uint16_t &y, adc_ref_t mode = MODE_DFR, uint8_t max_samples = 0xff) const;
+
+  void powerDown() const;
 
 private:
-	void update();
-	uint8_t csPin, tirqPin, rotation=1;
-	int16_t xraw=0, yraw=0, zraw=0;
-	uint32_t msraw=0x80000000;
-	SPIClass *_pspi = nullptr;
-#if defined(_FLEXIO_SPI_H_)
-	FlexIOSPI *_pflexspi = nullptr;
-#endif
+  static const uint8_t CTRL_LO_DFR = 0b0011;
+  static const uint8_t CTRL_LO_SER = 0b0100;
+  static const uint8_t CTRL_HI_X = 0b1001  << 4;
+  static const uint8_t CTRL_HI_Y = 0b1101  << 4;
+
+  static const uint16_t ADC_MAX = 0x0fff;  // 12 bits
+
+  uint16_t _width, _height;
+  rotation_t _rot;
+  uint8_t _cs_pin, _irq_pin;
+
+  int32_t _cal_dx, _cal_dy, _cal_dvi, _cal_dvj;
+  uint16_t _cal_vi1, _cal_vj1;
+
+  uint16_t _readLoop(uint8_t ctrl, uint8_t max_samples) const;
 };
 
-#ifndef ISR_PREFIX
-  #if defined(ESP8266)
-    #define ISR_PREFIX ICACHE_RAM_ATTR
-  #elif defined(ESP32)
-    // TODO: should this also be ICACHE_RAM_ATTR ??
-    #define ISR_PREFIX IRAM_ATTR
-  #else
-    #define ISR_PREFIX
-  #endif
-#endif
-
-#endif
+#endif  // _XPT2046_h_
